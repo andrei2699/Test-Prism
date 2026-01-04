ca22fe93d4b245aee7fd174db48c51be83d6e46b
Andrei Timar <atimar999@gmail.com>
2026-01-04 16:56:17 +0200

    ci: add dockerfile for UI
    closes #11

---
+++ ui/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+
+envsubst < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp && mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
+
+nginx -g 'daemon off;'
