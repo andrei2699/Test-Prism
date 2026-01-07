2236704538a7f112f6ed04402f8e536172188c4c
Andrei Timar <atimar999@gmail.com>
2026-01-07 16:48:19 +0200

    refactor(#46): update home component to use layout service

--- ui/src/app/pages/home/home.ts
+++ ui/src/app/pages/home/home.ts
@@ -1,9 +1,9 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LoadingComponent } from '../../components/loading/loading.component';
 import { ErrorMessageComponent } from '../../components/error-message/error-message.component';
 import { TestDataService } from '../../services/test-data.service';
-import { Layout, Page } from '../../types/Layout';
 import { PageRenderer } from '../../components/renderers/page-renderer/page-renderer';
+import { LayoutService } from '../../services/layout.service';
 
 @Component({
   selector: 'app-home',
@@ -14,39 +14,8 @@ import { PageRenderer } from '../../components/renderers/page-renderer/page-rend
 })
 export class Home {
   private testDataService = inject(TestDataService);
+  private layoutService = inject(LayoutService);
 
   testData = this.testDataService.testData;
-
-  layout = signal<Layout>({
-    dataSources: [
-      {
-        id: 'main',
-        url: 'http://localhost:8080/test-results/results.json',
-      },
-    ],
-    pages: [
-      {
-        title: 'Main Report',
-        path: '/',
-        widgets: [
-          {
-            id: 'overview-distribution-pie',
-            type: 'distribution-pie',
-            data: {
-              dataSourceId: 'main',
-            },
-          },
-          {
-            id: 'tests-tree',
-            type: 'tree',
-            data: {
-              dataSourceId: 'main',
-            },
-          },
-        ],
-      },
-    ],
-  });
-
-  currentPage = computed<Page>(() => this.layout().pages[0]);
+  layout = this.layoutService.layout;
 }
