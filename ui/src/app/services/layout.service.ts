2caa7cd68a7d65996622ca74245678b7550bf1d4
Andrei Timar <atimar999@gmail.com>
2026-01-07 16:23:49 +0200

    feat(#46): add layout service

---
+++ ui/src/app/services/layout.service.ts
@@ -0,0 +1,26 @@
+﻿import { inject, Injectable, resource } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
+import { Layout } from '../types/Layout';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LayoutService {
+  private http = inject(HttpClient);
+
+  layout = resource({
+    loader: async () => {
+      const path = this.getLayoutPath();
+      if (!path) {
+        throw new Error('LAYOUT_FILE environment variable is not set');
+      }
+      return lastValueFrom(this.http.get<Layout>(path));
+    },
+  });
+
+  private getLayoutPath(): string | null {
+    const windowEnv = (window as any)['ENV'];
+    return windowEnv?.LAYOUT_FILE || null;
+  }
+}
