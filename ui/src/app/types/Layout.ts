b582f5e0669afa3bf6154a1e545bdf52ac39c1dc
Andrei Timar <atimar999@gmail.com>
2026-01-07 14:37:27 +0200

    feat(#39): add layout type

---
+++ ui/src/app/types/Layout.ts
@@ -0,0 +1,39 @@
+﻿export interface Layout {
+  pages: Page[];
+  dataSources: DataSource[];
+}
+
+export interface Page {
+  title: string;
+  path: string;
+  widgets: Widget[];
+}
+
+export type WidgetType = 'tree' | 'distribution-pie' | 'analysis-summary';
+
+export interface Widget {
+  id: string;
+  type: WidgetType;
+  // TODO: type properties
+  properties?: Record<string, object>;
+  data: WidgetData;
+  style?: CSSStyleDeclaration;
+}
+
+export interface WidgetData {
+  dataSourceId: DataSourceId;
+  filter?: DataFilter;
+}
+
+export interface DataFilter {
+  // TODO: add missing properties
+}
+
+export type DataSourceId = string;
+
+export interface DataSource {
+  id: DataSourceId;
+  url: string;
+  headers?: Record<string, string>;
+  queryParams?: Record<string, string>;
+}
