badf2f2e8bca6265e1d9656a0020a18f19c39cd6
Andrei Timar <atimar999@gmail.com>
2026-07-04 10:46:14 +0300

    feat(#75): update test details to show test executions

--- ui/src/app/components/widgets/tree-widget/test-details-drawer/test-details-drawer.ts
+++ ui/src/app/components/widgets/tree-widget/test-details-drawer/test-details-drawer.ts
@@ -1,15 +1,16 @@
-﻿import { Component, computed, input, output } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
-import { Test } from '../../../../types/TestReport';
+import { DatePipe } from '@angular/common';
+import { Test, TestExecution } from '../../../../types/TestReport';
 import { HumanizeDurationPipe } from '../../../../pipes/humanize-duration.pipe';
 import { TestColors } from '../../../../types/Layout';
 import { getLastExecution } from '../../../../utils/testExecutionUtils';
 
 @Component({
   selector: 'app-test-details-drawer',
-  imports: [MatButtonModule, MatIconModule, MatChipsModule, HumanizeDurationPipe],
+  imports: [MatButtonModule, MatIconModule, MatChipsModule, HumanizeDurationPipe, DatePipe],
   templateUrl: './test-details-drawer.html',
   styleUrl: './test-details-drawer.css',
 })
@@ -19,6 +20,12 @@ export class TestDetailsDrawer {
   close = output<void>();
 
   lastExecution = computed(() => getLastExecution(this.test()));
+
+  sortedExecutions = computed<TestExecution[]>(() =>
+    this.test()
+      .executions.slice()
+      .sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()),
+  );
 
   testDuration = computed<number>(() => {
     return this.lastExecution()?.durationMs ?? 0;
