fea9280375306ca5d4fca146c5a30d7c6e148d1e
Andrei Timar <atimar999@gmail.com>
2026-07-04 08:05:58 +0300

    feat(deps): upgrade dependencies from ui and docs including angular 22

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -5,7 +5,7 @@ import {
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withXhr } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { AppConfigService } from './services/app-config.service';
@@ -14,7 +14,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withXhr()),
     provideCharts(withDefaultRegisterables()),
     provideAppInitializer(() => {
       const appConfigService = inject(AppConfigService);
